208607a58c8bf005ee379627728668279a8ce8c6
HaaZee <haazee4droid@gmail.com>
2018-03-09 18:35:53 +0100

    Készen van a méretében a rootéhoz igazodó Frame objektum

--- 2nd_ablak1.py
+++ 2nd_ablak1.py
@@ -7,22 +7,27 @@ Az első program másolata kibővítve egy Frame objektummal.
 Létrehozok a neten talált tutorialok alapján, immár csak az emlékeimre
 és a pythonos doksira támaszkodva egy Frame osztályból származtatott
 osztályt. Ebből készítek egy új objektumot, amit a "root" objektumhoz adok.
+
+Ahhoz, hogy a Frame mérete a root-éhoz igazodjon, a pack használata tűnik szükségesnek,
+ezen belül pedig az expand=Y és fill=BOTH paramétereké.
+A relief működéséhez a defaultnál vastagabb border szükséges (bd=3 pl.)
 """
 
 
 import tkinter as tk
 
+
 class MyApplication(tk.Frame):
 
     def __init__(self, root_window):
         super().__init__(root_window)
-        self.config(relief=tk.GROOVE, background="#AAAAFF", height=500, width=700)
-        self.grid(padx=5, pady=5, sticky="SE" )
+
 
 def app_start():
     root = tk.Tk()
-    #root.geometry("800x600")
     app_object = MyApplication(root)
+    app_object.config(relief=tk.GROOVE, bd=3, background="#AAAAFF", height=500, width=700)
+    app_object.pack(padx=5, pady=5, expand=tk.Y, fill=tk.BOTH)
     root.mainloop()
 
 
